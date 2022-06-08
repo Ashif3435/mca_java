9efe023541bc6c5b9afc13a4df19b203eb872e93
Ashif3425 <93881222+Ashif3435@users.noreply.github.com>
2022-06-08 16:31:44 +0530

    Add files via upload

--- files/file2.java
+++ files/file2.java
@@ -4,19 +4,20 @@ import java.io.FileWriter;
  class file2{
  	public static void main(String[] args) {
  		try{
+ 			int data;
  		File f1=new File("list.txt");
  		FileWriter ev=new FileWriter("even.txt");
  		FileWriter od=new FileWriter("odd.txt");
  		Scanner sn=new Scanner(f1);
 	 		while(sn.hasNextLine())
 	 		{
-	 			 int data = sn.nextInt();
+	 			 data = sn.nextInt();
 	 			 if(data%2==0)
 	 			 {
-	 			 	ev.write(data1);
+	 			 	ev.write(String.valueOf(data));
 	 			 }
 	 			 else{
-	 			 	od.write(data1);
+	 			 	od.write(String.valueOf(data));
 	 			 
 	 			 }
 	 			
